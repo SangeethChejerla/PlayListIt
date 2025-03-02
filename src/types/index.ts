5c77a0b57450be6ab5089d6c257f15ca49955594
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2025-03-03 00:16:30 +0530

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Track {
+    id: string
+    title: string
+    artist: string
+    album: string
+    url: string
+    duration: number
+    coverUrl: string
+    file?: File
+  }
+  
+  
